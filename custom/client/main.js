ebb7d7ab8d5852f837e28cd6206def8728d8e583
J3z <jeremaye@gmail.com>
2014-02-01 13:44:19 +0100

    Better interface but not yet finished and add the user display

---
+++ custom/client/main.js
@@ -0,0 +1,18 @@
+// on load of page
+$(function(){
+	// when the client clicks SEND
+	$('#datasend').click( function() {
+		var message = $('#data').val();
+		$('#data').val('');
+		// tell server to execute 'sendchat' and send along one parameter
+		socket.emit('sendchat', message);
+	});
+	// when the client hits ENTER on their keyboard
+	$('#data').keypress(function(e) {
+		if(e.which == 13) {
+			$(this).blur();
+			$('#datasend').focus().click();
+		}
+	});
+});
+ 
---
+++ custom/client/socket.js
@@ -0,0 +1,37 @@
+socket = io.connect('127.0.0.1:4224');
+
+	// on connection to server, ask for user's name with an anonymous callback
+	socket.on('connect', function(){
+		// call the server-side function 'adduser' and send one parameter (value of prompt)
+		socket.emit('adduser', prompt("Entrez votre nom"));
+	});
+
+	// listener, whenever the server emits 'updatechat', this updates the chat body
+	socket.on('updatechat', function (username, data) {
+		$('#conversation').append('<div class"chat-line row"><div class="col-md-1">'+username + ':</div><div class"col-md-11"> ' + data + '</div></div>');
+	});
+
+	// listener, whenever the server emits 'updaterooms', this updates the room the client is in
+	socket.on('updaterooms', function(rooms, current_room) {
+		$('#rooms').empty();
+		$.each(rooms, function(key, value) {
+			if(value == current_room){
+				$('#rooms').append('<div>' + value + '</div>');
+			}
+			else {
+				$('#rooms').append('<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>');
+			}
+		});
+	});
+
+	// listener, whenever the server emits 'updaterooms', this updates the room the client is in
+	socket.on('updateusers', function(users) {
+		$('#users').empty();
+		$.each(users, function(key, value) {
+			$('#users').append('<div><a href="#">' + value + '</a></div>');
+		});
+	});
+
+	function switchRoom(room){
+		socket.emit('switchRoom', room);
+	}
