bb212880e2d5f60ae84b3cfdad1760ec92a6f972
J3z <jeremaye@gmail.com>
2014-02-03 23:32:07 +0100

    Interface improvement

--- custom/client/main.js
+++ custom/client/main.js
@@ -1,17 +1,29 @@
 // on load of page
 $(function(){
+	
+	var buttonSend 	= $('#datasend');
+	var fieldSend	= $('#data');
+
 	// when the client clicks SEND
-	$('#datasend').click( function() {
-		var message = $('#data').val();
-		$('#data').val('');
+	buttonSend.click( function() {
+		var message = fieldSend.val();
+		fieldSend.val('');
 		// tell server to execute 'sendchat' and send along one parameter
 		socket.emit('sendchat', message);
 	});
 	// when the client hits ENTER on their keyboard
-	$('#data').keypress(function(e) {
+	$(window).keypress(function(e) {
 		if(e.which == 13) {
-			$(this).blur();
-			$('#datasend').focus().click();
+			if(fieldSend.is(":focus") && fieldSend.val() == ""){
+				fieldSend.blur();
+			}else if (fieldSend.is(":focus")){
+				buttonSend.click();
+				fieldSend.blur();
+			}else{
+				fieldSend.focus();
+			}
+		} else {
+			fieldSend.focus();
 		}
 	});
 });
--- custom/client/socket.js
+++ custom/client/socket.js
@@ -2,38 +2,44 @@ var url = window.location;
 
 socket = io.connect(url);
 
-	// on connection to server, ask for user's name with an anonymous callback
-	socket.on('connect', function(){
-		// call the server-side function 'adduser' and send one parameter (value of prompt)
-		socket.emit('adduser', prompt("Entrez votre nom"));
-	});
+// on connection to server, ask for user's name with an anonymous callback
+socket.on('connect', function(){
+	// call the server-side function 'adduser' and send one parameter (value of prompt)
+	socket.emit('adduser', prompt("Entrez votre nom"));
+});
 
-	// listener, whenever the server emits 'updatechat', this updates the chat body
-	socket.on('updatechat', function (username, data) {
-		$('#conversation').append('<div class"chat-line row"><div class="col-md-1">'+username + ':</div><div class"col-md-11"> ' + data + '</div></div>');
-	});
+// listener, whenever the server emits 'updatechat', this updates the chat body
+socket.on('updatechat', function (username, data) {
+	$('#conversation').append('<div class"chat-line row"><div class="col-md-1">'+ username + ':</div><div class"col-md-11"> ' + data + '</div></div>');
+	
 
-	// listener, whenever the server emits 'updaterooms', this updates the room the client is in
-	socket.on('updaterooms', function(rooms, current_room) {
-		$('#rooms').empty();
-		$.each(rooms, function(key, value) {
-			if(value == current_room){
-				$('#rooms').append('<div>' + value + '</div>');
-			}
-			else {
-				$('#rooms').append('<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>');
-			}
+	$('#conversation').animate({
+			scrollBottom: $('#conversation').prop('scrollHeight')
 		});
-	});
 
-	// listener, whenever the server emits 'updaterooms', this updates the room the client is in
-	socket.on('updateusers', function(users) {
-		$('#users').empty();
-		$.each(users, function(key, value) {
-			$('#users').append('<div><a href="#">' + value + '</a></div>');
-		});
-	});
+});
 
-	function switchRoom(room){
-		socket.emit('switchRoom', room);
-	}
+// listener, whenever the server emits 'updaterooms', this updates the room the client is in
+socket.on('updaterooms', function(rooms, current_room) {
+	$('#rooms').empty();
+	$.each(rooms, function(key, value) {
+		if(value == current_room){
+			$('#rooms').append('<div>' + value + '</div>');
+		}
+		else {
+			$('#rooms').append('<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>');
+		}
+	});
+});
+
+// listener, whenever the server emits 'updaterooms', this updates the room the client is in
+socket.on('updateusers', function(users) {
+	$('#users').empty();
+	$.each(users, function(key, value) {
+		$('#users').append('<div><a href="#">' + value + '</a></div>');
+	});
+});
+
+function switchRoom(room){
+	socket.emit('switchRoom', room);
+}
